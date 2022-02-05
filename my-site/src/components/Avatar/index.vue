7bbaf386319979e6a5f5a2c402da379775a69fd2
szg <576804093@163.com>
2022-02-06 01:33:04 +0800

    添加elementUI;增加头像组件

---
+++ my-site/src/components/Avatar/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src="url" alt=""/>
+</template>
+
+<script>
+export default {
+  name: 'Avatar', // 头像组件
+  props: {
+    url: {
+      type: String,
+      required: true, // 属性必填
+      default: '',
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- my-site/src/views/Home.vue
+++ my-site/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Avatar url="USS"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Avatar from '@/components/Avatar/index';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Avatar,
   },
 };
 </script>
